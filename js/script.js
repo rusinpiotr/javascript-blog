dc328636c5fc2eaab2dda96228f5bf18d817d9ef
Piotr Rusin <rusin@stonearts.pl>
2026-05-19 22:20:22 +0200

    prevent default link behavior in title click handler

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 "use strict";
-// document.getElementById("test-button").addEventListener("click", function () {
-//   const links = document.querySelectorAll(".titles a");
-//   console.log("links:", links);
-// });
 
 const titleClickHandler = function (event) {
-  event.preventDefault();
+    event.preventDefault();
+    const clickedElement = this;
   console.log("Link was clicked!");
   console.log("event:", event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
-/* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add("active");
+  console.log('clickedElement:', clickedElement);
 
-/* remove class 'active' from all articles */
+/* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts .post.active");
 
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove("active");
   }
-}
+
 /* get 'href' attribute from the clicked link */
 
 /* find the correct article using the selector (value of 'href' attribute) */
 
 /* add class 'active' to the correct article */
+}
 
 const links = document.querySelectorAll(".titles a");
 
