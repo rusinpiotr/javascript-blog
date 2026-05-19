e24d7eb4102a338df39378fc7eaff1830a9afddd
Piotr Rusin <rusin@stonearts.pl>
2026-05-19 21:04:24 +0200

    remove article class from all axtive articles on title ckick

--- js/script.js
+++ js/script.js
@@ -4,26 +4,34 @@
 //   console.log("links:", links);
 // });
 
-const titleClickHandler = function(event){
-    event.preventDefault();
-  console.log('Link was clicked!');
-  console.log('event:', event);
-}
+const titleClickHandler = function (event) {
+  event.preventDefault();
+  console.log("Link was clicked!");
+  console.log("event:", event);
 
-/* remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
 
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
 /* add class 'active' to the clicked link */
 
 /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".posts .post.active");
 
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
+}
 /* get 'href' attribute from the clicked link */
 
 /* find the correct article using the selector (value of 'href' attribute) */
 
 /* add class 'active' to the correct article */
 
-const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll(".titles a");
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
 }
